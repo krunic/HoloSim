4ecb192b713082b77ce41b40437c3dff19861bee
Veljko Krunic <krunic@ieee.org>
2010-02-06 23:10:19 +0000

    Reenabled unit tests. When tests pass, Z buffer based calculation is finished

--- UnitTests/CPPUnit/UnitTests.cpp
+++ UnitTests/CPPUnit/UnitTests.cpp
@@ -17,7 +17,6 @@ int main( int argc, char **argv)
    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
    runner.addTest(registry.makeTest());
 
-//   bool wasSuccessful = runner.run("", false);
-//   return !wasSuccessful;
-   return false;
+   bool wasSuccessful = runner.run("", false);
+   return !wasSuccessful;
 }
